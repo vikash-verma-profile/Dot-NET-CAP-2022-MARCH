750be49e82d355bce05de073f8f0fe4c9cf57758
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-08 15:23:28 +0530

    Loops

--- ConsoleApp1/ConsoleApp1/ConditionalStatements.cs
+++ ConsoleApp1/ConsoleApp1/ConditionalStatements.cs
@@ -52,25 +52,61 @@ namespace ConsoleApp1
            string meessage= (10 > 5) ? "Hi this is greater than 5" : "This is less than 5";
             Console.WriteLine(meessage);
 
-            Console.WriteLine("Please enter some choice");
-            int WeekdayNumber=Convert.ToInt32(Console.ReadLine());
-            //switch
-            switch (WeekdayNumber)
-            {
-                case 1:
-                    Console.WriteLine("Today is Sunday");
-                    break;
-                case 2:
-                    Console.WriteLine("Today is Monday");
-                    break;
-                case 3:
-                    Console.WriteLine("Today is Tuesday");
-                    break;
-                default:
-                    Console.WriteLine("Please enter the correct choice");
-                    break;
-            }
+            //Console.WriteLine("Please enter some choice");
+            //int WeekdayNumber=Convert.ToInt32(Console.ReadLine());
+            ////switch
+            //switch (WeekdayNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Today is Sunday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Today is Monday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Today is Tuesday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Please enter the correct choice");
+            //        break;
+            //}
 
+            //loops
+            //for loop
+            //while
+            //do while
+            //for each
+            //for (<intialize part>;<conditional part>;<Increamet/decement>)
+            Console.WriteLine("==========For Loop===============");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("==========while Loop===============");
+            int j = 0;
+            //Entry controlled loop
+            while (j < 5)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            Console.WriteLine("==========Do While===============");
+            //Exit controlled loop
+            int k = 0;
+            do 
+            {
+                Console.WriteLine(k);
+                k++;
+            } while (k < 5) ;
+            Console.WriteLine("==========Nesting Loops===============");
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
