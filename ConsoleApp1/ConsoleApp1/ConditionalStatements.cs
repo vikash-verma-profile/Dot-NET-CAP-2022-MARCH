6ceec06a0335b4d8e427e63c822486000982ebcc
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-08 14:12:58 +0530

    Ternary operator

--- ConsoleApp1/ConsoleApp1/ConditionalStatements.cs
+++ ConsoleApp1/ConsoleApp1/ConditionalStatements.cs
@@ -8,7 +8,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            //int SampleNumber=Convert.ToInt32(Console.ReadLine());
+            ////check a number is postive or negative and also check for whole number
+            //if (SampleNumber > 0) //if your expression is true
+            //{
+            //    //then this part will run
+            //    Console.WriteLine("Number is a postive number");
+            //}
+            //else if(SampleNumber==0)
+            //{
+            //    //in false case this part will run
+            //    Console.WriteLine("Number is a Whole Number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is a Negative Number");
+            //}
+
+
+            //int i;
+            //Console.Write("Enter a Number : ");
+            //i = int.Parse(Console.ReadLine());
+            //if (i > 0 && i < 18)
+            //{
+            //    Console.Write("Entered age is not eligible for vote");
+            //    Console.Read();
+            //}
+            //else if (i >= 18 && i < 60)
+            //{
+            //    Console.Write("Entered age is eligible for vote");
+            //    Console.Read();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Persion is senior citizen");
+            //    Console.Read();
+            //}
+
+            //?:
+            //<expersion>?true:false;
+            //if(){}else{}
+            //()?{}:{}
+           string meessage= (10 > 5) ? "Hi this is greater than 5" : "This is less than 5";
+            Console.WriteLine(meessage);
+
         }
     }
 }
