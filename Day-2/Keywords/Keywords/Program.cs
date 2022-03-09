9050711f1771c8258916e7716c6e66427e8585ca
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-09 09:45:12 +0530

    added arrays

---
+++ Day-2/Keywords/Keywords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Keywords
+{
+    internal class Program
+    {
+        //collection of homogeneous type of data
+        static void Main(string[] args)
+        {
+            //<data-type> [] <identfier>
+            int[] Numbers;
+            int[] NumbersList = {1,2,3,89,90,100};
+
+            int length = NumbersList.Length;
+            //6
+            for (int index = 0; index < length; index++)
+            {
+                Console.WriteLine(NumbersList[index]);
+            }
+            Console.WriteLine("================For-Each========================");
+            foreach (var item in NumbersList)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+}
