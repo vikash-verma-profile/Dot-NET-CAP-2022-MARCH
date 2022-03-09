81f4bbfa6aedd709eb261075411fa2a5afec3868
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-09 17:24:50 +0530

    first commit

--- Day-2/Keywords/Keywords/NullableType.cs
+++ Day-2/Keywords/Keywords/NullableType.cs
@@ -9,18 +9,31 @@ namespace Keywords
         static void Main(string[] args)
         {
             //null
-            Nullable<int> i = null;
-            int ?k = null;
+            //Nullable<int> i = null;
+            //int ?k = null;
 
-            int samplevalue = int.Parse("3");
-            Console.WriteLine(samplevalue);
-            int result;
-            bool sample = int.TryParse("asd",out result);
-            Console.WriteLine(result);
-            Console.WriteLine(sample);
+            //int samplevalue = int.Parse("3");
+            //Console.WriteLine(samplevalue);
+            //int result;
+            //bool sample = int.TryParse("asd",out result);
+            //Console.WriteLine(result);
+            //Console.WriteLine(sample);
 
             //create a program to handle if user enter any value apart from int
             //you should show error that please enter correct integer value
+            Console.WriteLine("enter some value");
+            string sampleValue=Console.ReadLine();
+
+            int result;
+            bool IsIntValue = int.TryParse(sampleValue, out result);
+            if (IsIntValue)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Enter the value in integer format");
+            }
         }
     }
 }
