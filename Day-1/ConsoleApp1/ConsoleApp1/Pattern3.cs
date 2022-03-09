e9a5aee77728aba298221786f4813435075a6aa7
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-09 09:29:49 +0530

    added problem statement

---
+++ Day-1/ConsoleApp1/ConsoleApp1/Pattern3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Pattern3
+    {
+        static void Main(string[] args)
+        {
+            int max = 5;
+
+            //This part is printing top level block
+            for (var i = 1; i <= max; i++)
+            {
+                for (var j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var k = i; k <= max; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            //This part is printing lower level block
+            for (var i = max; i >= 1; i--)
+            {
+                for (var j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var k = i; k <= max; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Day-1/ConsoleApp1/ConsoleApp1/PrintPattern.cs
+++ Day-1/ConsoleApp1/ConsoleApp1/PrintPattern.cs
@@ -6,22 +6,25 @@ namespace ConsoleApp1
 {
     internal class PrintPattern
     {
-        static void Main(string[] args)
+        static void Main3(string[] args)
         {
             int space = 0;
             int max = 5;
 
+            //this is runner 
             for (var i = max; i > 0; i--)
             {
+                //this is printing Left side *  
                 for (var j = 0; j < i; j++)
                 {
                     Console.Write("*");
                 }
+                //this is creating spaces
                 for (var j = 0; j < space; j++)
                 {
                     Console.Write(" ");
                 }
-
+                //this is priniting right side *'s
                 for (var j = 0; j < i; j++)
                 {
                     Console.Write("*");
