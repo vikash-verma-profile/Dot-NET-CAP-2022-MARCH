770fff4c193904d33e8037a77b55e0f997cd588c
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-10 11:34:15 +0530

    first commit

---
+++ Day-3/ConsoleApp1/ConsoleApp1/ClassDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public int StudentRollNo { get; set; }
+        public string StudentName { get; set; }
+        public string StudentAddress { get; set; }
+        public int StudentAge { get; set; }
+
+        public void GetStudentData()
+        {
+            Console.WriteLine("Please enter student Roll Number");
+            StudentRollNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter student Name");
+            StudentName = Console.ReadLine();
+            Console.WriteLine("Please enter student Address");
+            StudentAddress = Console.ReadLine();
+            Console.WriteLine("Please enter student Age");
+            StudentAge = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayStudentData()
+        {
+            Console.WriteLine("Student Details are :-");
+            Console.WriteLine("Student Name : " + StudentName);
+            Console.WriteLine("Student Roll No : " + StudentRollNo);
+            Console.WriteLine("Student Address : " + StudentAddress);
+            Console.WriteLine("Student Age : " + StudentAge);
+        }
+    }
+    internal class ClassDemo
+    {
+        public static void Main()
+        {
+            Student student = new Student();
+            student.GetStudentData();
+            student.DisplayStudentData();
+
+        }
+    }
+}
--- Day-3/ConsoleApp1/ConsoleApp1/String-functions.cs
+++ Day-3/ConsoleApp1/ConsoleApp1/String-functions.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 {
     internal class String_functions
     {
-        public   static void Main()
+        public   static void Main3()
         {
 
             //string 1 > string 2 ==>1
@@ -15,9 +15,9 @@ namespace ConsoleApp1
             string cstring1 = "1234";
             string cstring2 = "1234";
             string cstring3 = "12345";
-            Console.WriteLine(string.Compare(cstring1, cstring2));
-            Console.WriteLine(string.Compare(cstring3, cstring2));
-            Console.WriteLine(string.Compare(cstring2, cstring3));
+            Console.WriteLine(string.Compare(cstring1, cstring2));//0
+            Console.WriteLine(string.Compare(cstring3, cstring2)); //1
+            Console.WriteLine(string.Compare(cstring2, cstring3)); //-1
 
 
             Console.WriteLine("=========== ===============");
@@ -26,24 +26,32 @@ namespace ConsoleApp1
             string sample1 = "hello";
 
             string sample2 = "hello";
-            string sample3 = "cshrap";
+            string sample123 = "cshrap";
 
             string sample4 = "mello";
             string sample5 = "hello";
             //lowe case
-            sample =sample.ToLower();
-            Console.WriteLine("===========Lower case ===============");
-            Console.WriteLine(sample);
-            sample=sample.ToUpper();
-            Console.WriteLine("===========Upper case ===============");
-            Console.WriteLine(sample);
-            Console.WriteLine("===========Compare case ===============");
+            //sample =sample.ToLower();
+            //Console.WriteLine("===========Lower case ===============");
+            //Console.WriteLine(sample);
+            //sample=sample.ToUpper();
+            //Console.WriteLine("===========Upper case ===============");
+            //Console.WriteLine(sample);
+            //Console.WriteLine("===========Compare case ===============");
             //var result=string.Compare(sample, sample2);
-            Console.WriteLine(string.Compare(sample1, sample2)); //s1==s2 //0 when both length and content matches
-            
-            Console.WriteLine(string.Compare(sample2, sample3)); //  //1 when both length and content matches
-            Console.WriteLine(string.Compare(sample3, sample4)); //-1when both length and content doesnot match matches
-            Console.WriteLine(string.Compare(sample4, sample5)); //1
+            //Console.WriteLine(string.Compare(sample1, sample2)); //s1==s2 //0 when both length and content matches
+            Console.WriteLine("=========== ===============");
+            //Console.WriteLine(sample2.Length);
+            //Console.WriteLine(sample123.Length);
+            Console.WriteLine("=========== ===============");
+            //Console.WriteLine(sample2.Length> sample123.Length?1:-1);
+            Console.WriteLine(string.Compare("Hello1", "Hello")); //firststring > second string 1
+            Console.WriteLine(string.Compare("Hello", "Hello1"));//firststring < second string -1
+
+            Console.WriteLine("=========== ===============");
+            ////  //1 when both length and content matches
+            //Console.WriteLine(string.Compare(sample3, sample4)); //-1when both length and content doesnot match matches
+            //Console.WriteLine(string.Compare(sample4, sample5)); //1
             //1
             //-1
             //0
