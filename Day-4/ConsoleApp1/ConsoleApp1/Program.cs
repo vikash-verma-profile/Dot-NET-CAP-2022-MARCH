4982c6b3b588b696dad10e7c60d5e6a2f3e3f63d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-11 09:28:42 +0530

    added code for problem statement

---
+++ Day-4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        protected int age;
+        public void Greet()
+        {
+            Console.WriteLine("Hello !");
+        }
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+    }
+    class Student:Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I am Studying");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is {0} years old",age);
+        }
+
+    }
+    class Teacher:Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();   
+            person.Greet();
+
+            Student student = new Student();
+            student.SetAge(20);
+            student.Greet();
+            student.ShowAge();
+
+            Teacher teacher = new Teacher();
+            teacher.SetAge(30);
+            teacher.Greet();
+            teacher.Explain();
+        }
+    }
+}
