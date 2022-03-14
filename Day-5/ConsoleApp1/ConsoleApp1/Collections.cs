04ee37c3ed1edbdba259c69afec23f8539a50398
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-03-14 15:21:35 +0530

    first commit

--- Day-5/ConsoleApp1/ConsoleApp1/Collections.cs
+++ Day-5/ConsoleApp1/ConsoleApp1/Collections.cs
@@ -41,6 +41,7 @@ namespace ConsoleApp1
                 Console.WriteLine(item);
             }
 
+            Console.WriteLine("===============Hash  Table===============");
             //HashTable
             //key and value
             //x==>778
@@ -49,12 +50,64 @@ namespace ConsoleApp1
             ht.Add("ora","oracle");
             ht.Add("vb", "vb.net");
             ht.Add("cs", "cs.net");
+            ht.Add("asp", "asp.net");
 
             foreach (DictionaryEntry item in ht)
             {
                 Console.WriteLine(item.Key+"===>"+item.Value);
 
                 //Console.WriteLine(item);
+            }
+
+
+            //sortedList
+            /*
+             * its a combination of arraylist and hashtable
+             * arrange all the items in sorted order
+             */
+
+            Console.WriteLine("===============Sorted List===============");
+            SortedList sl=new SortedList();
+            sl.Add("ora", "oracle");
+            sl.Add("vb", "vb.net");
+            sl.Add("cs", "cs.net");
+            sl.Add("asp", "asp.net");
+            foreach (DictionaryEntry item in sl)
+            {
+                Console.WriteLine(item.Key + "===>" + item.Value);
+
+                //Console.WriteLine(item);
+            }
+
+            //Stack
+            /*
+             * LIFO
+             */
+            Console.WriteLine("===============Stack===============");
+            Stack stk=new Stack();
+            stk.Push("cs.net");
+            stk.Push("vb.net");
+            stk.Push("asp.net");
+            stk.Push("sqlserver");
+            foreach (var item in stk)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("===============Queue===============");
+            //Queue
+            /*
+            * FIFO
+            */
+
+            Queue q = new Queue();
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            q.Enqueue("sqlserver");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
             }
         }
     }
